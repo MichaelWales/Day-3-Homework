4be010f1f77b3d2d435eb2a02c945782e68709e1
Michael Wales <michaelwales@gmail.com>
2020-03-11 18:46:52 +0000

    Adds a solution to excercise A problem #8

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -15,5 +15,7 @@ stops.slice!(2)
 #7. Print the number of stops there are in the array?
 p stops.length
 #8. Show as many ways as you can to return "Falkirk High" from the array?
+p stops[2]
+p stops.index("Falkirk High")
 #9. Reverse the positions of the stops in the array
 #10 Print out all the stops using a for loop
