60b75d4ca01d22c6dd6ebb721ab20465139d02b2
Michael Wales <michaelwales@gmail.com>
2020-03-11 19:47:28 +0000

    Adds a solution to problem #8

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -69,7 +69,8 @@ even_numbers = users["Avril"][:lottery_numbers]
 p even_numbers.select(&:even?)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"][:lottery_numbers] << 7
-p users["Erik"][:lottery_numbers]
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+p users["Erik"][:home_town]
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
